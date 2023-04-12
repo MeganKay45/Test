c6ca366d2d0710da70bb9f3606a3b646ea7c9c85
MeganKay45 <130116352+MeganKay45@users.noreply.github.com>
2023-04-11 19:28:05 -0600

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,23 @@
+userName = input("Enter user name: ")
+passWord = input("Enter Password: ")
+_DOB = input("Enter Date (MMDDYY): ")
+input("\n Please press enter or return to quit the program")
+
+item3 = f"Hi, my name is {userName} and I was born on {_DOB}"
+print(item3)
+##Comparison operators
+res1 = 18 < 15
+print(f"is 18 smaller than 15?: {res1}")
+res4 = "Cat" != "Dog"
+print(f"Are the strings not identical? {res4}")
+
+firstNumber = input("Enter first number: ")
+secondNumber = input("Enter second number: ")
+result = int(firstNumber) * int(secondNumber)
+print(result)
+##this is typecasting options
+thirdNumber = int(input("Enter 3rd number: "))
+fourthNumber = int(input("Enter 4th number: "))
+result2 = thirdNumber + fourthNumber
+print(result2)
+
